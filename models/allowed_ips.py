83285a52f2d77cbd7040501d666e5383b80d6fb6
Giovanni D'Amato <damato.g@stesi.consulting>
2024-03-14 17:59:58 +0100

    [UPLOAD] 17.0

--- models/allowed_ips.py
+++ models/allowed_ips.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+###############################################################################
 #
 #    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Niyas Raphy(<https://www.cybrosys.com>)
-#    you can modify it under the terms of the GNU LESSER
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author:  Mruthul Raj (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
 #    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-
+#
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
 #
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    GENERAL PUBLIC LICENSE (AGPL v3) along with this program.
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
 #    If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-from odoo import models, fields
-
-
-class ResUsersInherit(models.Model):
-    _inherit = 'res.users'
-
-    allowed_ips = fields.One2many('allowed.ips', 'users_ip', string='IP')
+###############################################################################
+from odoo import fields, models
 
 
 class AllowedIPs(models.Model):
+    """Class for the model allowed_ips. Contains fields for record users and
+    their Allowed IPs."""
     _name = 'allowed.ips'
+    _description = "Allowed IPs"
 
-    users_ip = fields.Many2one('res.users', string='IP')
-    ip_address = fields.Char(string='Allowed IP')
+    user_ip_id = fields.Many2one('res.users', string='User',
+                                 help='User associated with the allowed IP')
+    ip_address = fields.Char(string='Allowed IP', help='The allowed IP address'
+                                                       ' for the User.')
--- models/res_users.py
+++ models/res_users.py
@@ -19,4 +19,13 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
-from . import access_restriction_by_ip
+from odoo import fields, models
+
+
+class ResUsersInherit(models.Model):
+    """Inherited res_users for adding new field allowed ip_ids"""
+    _inherit = 'res.users'
+
+    allowed_ip_ids = fields.One2many('allowed.ips', 'user_ip_id',
+                                     string='IP Address',
+                                     help="Allowed ip addresses for the user.")
